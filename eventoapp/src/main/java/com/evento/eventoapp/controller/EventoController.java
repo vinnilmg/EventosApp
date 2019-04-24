d91760a2764afd3699ffde254038207e00156d7b
Vini <VLG0219@TERC-AE3DU999.Sulamerica.br>
2019-04-24 18:14:18 -0300

    24-04 Create eventos funcionando

--- eventoapp/src/main/java/com/evento/eventoapp/controller/EventoController.java
+++ eventoapp/src/main/java/com/evento/eventoapp/controller/EventoController.java
@@ -1,7 +1,12 @@
 package com.evento.eventoapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.evento.eventoapp.model.Evento;
+import com.evento.eventoapp.repository.EventoRepository;
 
 @Controller
 public class EventoController {
@@ -10,9 +15,21 @@ public class EventoController {
 //	public String index() {
 //		return "index";
 //	}
-	@RequestMapping("/cadastrarEvento")
+	
+	@Autowired
+	private EventoRepository eventoRepository;
+	
+	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.GET)
 	public String form() {
 		return "evento/formEvento";
+	}
+
+	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.POST)
+	public String form(Evento evento) {
+		
+		eventoRepository.save(evento);
+		
+		return "redirect:/cadastrarEvento";
 	}
 
 }
